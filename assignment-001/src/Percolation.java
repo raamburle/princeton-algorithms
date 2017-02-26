219a0e938e43f44bd6f81bcc49752812c1b8d028
Rohan <rohanamburle@gmail.com>
2017-02-25 22:34:13 -0600

    Added class implementations

--- assignment-001/src/Percolation.java
+++ assignment-001/src/Percolation.java
@@ -1,27 +1,215 @@
+import edu.princeton.cs.algs4.WeightedQuickUnionUF;
+
+import java.util.Arrays;
+
 /**
+ * Percolation is a class used to store a simple NxN grid
+ * This grid can be used to calculate the percolation threshold
+ * using the Monte Carlo simulation
  *
+ * @author Rohan Amburle
+ * @version %I%, %G%
+ * @since 1.0
  */
 public class Percolation {
+    // Size of the grid
+    private final int gridSize;
+    // Total number of open sites at any moment
+    private int numberOfOpenSites;
+
+    // Dummy head site
+    private final int HEAD_SITE;
+    // Dummy tail site
+    private final int TAIL_SITE;
+
+    // Grid storing open/close state of individual sites
+    private boolean[][] grid;
+
+    // Union/find structure used to maintain site connections in the grid
+    WeightedQuickUnionUF siteTree;
+
+    /**
+     * Constructor
+     *
+     * @param n (required) Size of the grid. Has to be greater than zero
+     */
     Percolation(int n) {
 
+        // Check if grid size is legal and create the grid.
+        if (n < 1) {
+            throw new IllegalArgumentException
+                    ("Invalid grid size. Must be greater than zero");
+        }
+        gridSize = n;
+
+        grid = new boolean[gridSize][gridSize];
+
+        // Close all the sites. false is closed. true is open
+        // Not necessary as default is false, but is a good practice
+        for (int i = 1; i <= gridSize; i++) {
+            for (int j = 1; j <= gridSize; j++) {
+                grid[i - 1][j - 1] = false;
+            }
+        }
+
+        numberOfOpenSites = 0;  // No open sites to begin with
+        // TODO - Remove all SysOuts
+        //System.out.println(Arrays.deepToString(grid));
+
+        // Create site tree structure and calculate top/bottom site indices
+
+        siteTree = new WeightedQuickUnionUF(gridSize * gridSize + 2);
+        HEAD_SITE = 0;
+        TAIL_SITE = gridSize * gridSize + 1;
     }
+
+    /**
+     * Open the site at index [row, col] and connect it with the
+     * adjoining open sites
+     *
+     * @param row (required) The row index of the site being opened.
+     *            index must be between [1,gridSize]
+     * @param col (required) The column index of the site being opened.
+     *            index must be between [1,gridSize]
+     */
     public void open(int row, int col) {
+        // No need to check arguments here as isOpen() is being called
 
+        if (isOpen(row, col) != true) {
+            grid[row - 1][col - 1] = true;
+            numberOfOpenSites++;    // One more site opened up
+
+            // Calculate indices of involved sites
+
+            int currentSite = (row - 1) * gridSize + col;
+
+            // Connect to top site if open
+            if (row == 1) {
+                // Site in top row. Connect to head site
+                siteTree.union(currentSite, HEAD_SITE);
+            } else {
+                if (isOpen(row - 1, col)) {
+                    siteTree.union(currentSite, currentSite - gridSize);
+                }
+            }
+
+            // Connect to bottom site if open
+            if (row == gridSize) {
+                // Site in bottom row. Connect to tail site
+                siteTree.union(currentSite, TAIL_SITE);
+            } else {
+                if (isOpen(row + 1, col)) {
+                    siteTree.union(currentSite, currentSite + gridSize);
+                }
+            }
+
+            // Connect to left and right site if not in the boundary rows
+            if (row != 1 && row != gridSize) {
+                if (col != 1 && isOpen(row, col - 1)) {
+                    siteTree.union(currentSite, currentSite - 1);
+                }
+                if (col != gridSize && isOpen(row, col + 1)) {
+                    siteTree.union(currentSite, currentSite + 1);
+                }
+            }
+
+        } else {
+            // TODO - Remove this block after coding complete
+            //System.out.println("Already open");
+        }
     }
+
+    /**
+     * Check if site at index [row, col] is open.
+     *
+     * @param row (required) The row index of the site being checked.
+     *            index must be between [1,gridSize]
+     * @param col (required) The column index of the site being checked.
+     *            index must be between [1,gridSize]
+     * @return <code>true</code> if the site is open;
+     * <code>false</code> otherwise
+     */
     public boolean isOpen(int row, int col) {
+        checkArguments("Grid indices", gridSize, row, col);
 
+        return grid[row - 1][col - 1];
     }
+
+    /**
+     * Check if site at index [row, col] is connected to an open
+     * site in the top row.
+     *
+     * @param row (required) The row index of the site being checked.
+     *            index must be between [1,gridSize]
+     * @param col (required) The column index of the site being checked.
+     *            index must be between [1,gridSize]
+     * @return <code>true</code> if the site is full;
+     * <code>false</code> otherwise
+     */
     public boolean isFull(int row, int col) {
+        checkArguments("Grid indices", row, col);
 
+        return siteTree.connected((row - 1) * gridSize + col, HEAD_SITE);
     }
+
+    /**
+     * Returns the number of currently open sites in the grid
+     *
+     * @return Number of currently open sites in the grid
+     */
     public int numberOfOpenSites() {
-
+        return numberOfOpenSites;
     }
+
+    /**
+     * Checks if the current grid percolates
+     *
+     * @return <code>true</code> if the system percolates;
+     * <code>false</code> otherwise
+     */
     public boolean percolates() {
-
+        return siteTree.connected(HEAD_SITE, TAIL_SITE);
     }
 
-    public static void main(String[] args) {
+    /**
+     * Common method to validate if numeric arguments are valid
+     * i.e. 1 &lt;= arg &lt;= grid size
+     *
+     * @param paramName (required) A simple description of the parameter to
+     *                  print in the exception message
+     * @param args      (required) A <code>vararg</code> list of integers
+     *                  to be validated
+     */
+    private static void checkArguments(String paramName,
+                                       int gridSize, int... args) {
+        for (int arg : args) {
+            if (arg < 1 || arg > gridSize) {
+                throw new IllegalArgumentException
+                        (paramName + " invalid" + "value: " + arg);
+            }
+        }
+    }
 
+    // TODO - Remove main after coding complete
+    public static void main(String[] args) {
+        //int n = Integer.parseInt(args[0]);
+        int n = 6;
+        Percolation grid = new Percolation(n);
+        grid.open(1, 2);
+        System.out.println(grid.percolates());
+        grid.open(2, 2);
+        System.out.println(grid.percolates());
+        grid.open(2, 3);
+        System.out.println(grid.percolates());
+        grid.open(3, 3);
+        System.out.println(grid.percolates());
+        grid.open(4, 3);
+        System.out.println(grid.percolates());
+        grid.open(5, 2);
+        System.out.println(grid.percolates());
+        grid.open(5, 3);
+        System.out.println(grid.percolates());
+        grid.open(6, 2);
+        System.out.println(grid.percolates());
     }
 }
--- assignment-001/src/PercolationStats.java
+++ assignment-001/src/PercolationStats.java
@@ -1,24 +1,88 @@
+import edu.princeton.cs.algs4.StdRandom;
+import edu.princeton.cs.algs4.StdStats;
+
+import static java.lang.System.exit;
+
 /**
+ * PercolationStats class is used to capture percolation threshold
+ * using Monte Carlo simulation and capture statistical data
  *
+ * @author Rohan Amburle
+ * @version %I%, %G%
+ * @since 1.0
  */
 public class PercolationStats {
 
+    // Maintains the number of sites at percolation for each run
+    private double stats[];
+
+    // No. of trials in this experiment
+    private int trials;
+
+    /**
+     * Constructor
+     *
+     * @param n      (required) Size of the grid. Has to be greater than zero
+     * @param trials (required) No. of independent trials to be performed.
+     *               Has to be greater than zero
+     */
     public PercolationStats(int n, int trials) {
 
+        if (n <= 0 || trials <= 0) {
+            throw new IllegalArgumentException("Grid size and no. of " +
+                    "trials must be greater than zero");
+        }
+
+        this.trials = trials;
+        stats = new double[trials];
+        for (int i = 0; i < trials; i++) {
+            stats[i] = 0;
+
+            Percolation grid = new Percolation(n);
+
+            while (!grid.percolates()) {
+                // Open a random site
+                grid.open(StdRandom.uniform(n) + 1, StdRandom.uniform(n) + 1);
+            }
+
+            stats[i] = (double) grid.numberOfOpenSites() / (n * n);
+        }
     }
+
     public double mean() {
-
+        return StdStats.mean(stats);
     }
+
     public double stddev() {
-
+        return StdStats.stddev(stats);
     }
+
     public double confidenceLo() {
+        return (mean() - 1.96 * stddev() / Math.sqrt(trials));
+    }
 
-    }
     public double confidenceHi() {
-        
+        return (mean() + 1.96 * stddev() / Math.sqrt(trials));
     }
+
     public static void main(String[] args) {
 
+        if (args.length != 2) {
+            System.out.println("Invalid arguments");
+            System.out.println("\nUsage:\n\nPercolationStats " +
+                    "<grid size> <no. of trials>");
+            exit(1);
+        }
+
+        int n = Integer.parseInt(args[0]);            // grid size
+        int trials = Integer.parseInt(args[1]);       // no. of trials
+
+        PercolationStats experiment = new PercolationStats(n, trials);
+        //System.out.println(Arrays.toString(experiment.stats));
+        System.out.println("Mean                   : " + experiment.mean());
+        System.out.println("StdDev                 : " + experiment.stddev());
+        System.out.println("95% confidence interval: ["
+                + experiment.confidenceLo() + ", "
+                + experiment.confidenceHi() + "]");
     }
 }
