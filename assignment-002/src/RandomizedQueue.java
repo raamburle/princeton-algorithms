1f7dcee8afebcd937bb5f19c7e252ae998534237
Rohan <rohanamburle@gmail.com>
2017-03-09 14:41:48 -0600

    Added RandomizedQueue API signature

--- assignment-002/src/RandomizedQueue.java
+++ assignment-002/src/RandomizedQueue.java
@@ -1,9 +1,35 @@
+import java.util.Iterator;
+
 /**
- *
- *
+ * Implementation of a randomized queue
+ * 
  * @author Rohan Amburle
  * @version %I%, %G%
  * @since 1.0
  */
-public class RandomizedQueue {
+public class RandomizedQueue<Item> implements Iterable<Item> {
+    public RandomizedQueue() {
+    }
+
+    public boolean isEmpty() {
+    }
+
+    public int size() {
+    }
+
+    public void enqueue(Item item) {
+    }
+
+    public Item dequeue() {
+    }
+
+    public Item sample() {
+    }
+
+    @Override
+    public Iterator<Item> iterator() {
+    }
+
+    public static void main(String[] args) {
+    }
 }
